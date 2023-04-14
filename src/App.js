6c946b16e12e4b1206b142c83c1c1f01ac907d63
leen bassam <leen199xx@gmail.com>
2023-04-14 04:58:14 -0800

    2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { Routes, BrowserRouter, Route, Router } from "react-router-dom";
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dahboard";
+import Protected from "./Protected";
 function App() {
   return (
-    <div>
-      <Login />
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route
+            path="/dashboard"
+            element={
+              <Protected>
+                <Dashboard />
+              </Protected>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,76 +1,115 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Link,
   Typography,
+  IconButton,
   Button,
   Grid,
   Paper,
   InputAdornment,
   TextField,
   Avatar,
+  Container,
 } from "@mui/material";
+import axios from "axios";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import VisibilityIcon from "@mui/icons-material/Visibility";
 import MailOutlineIcon from "@mui/icons-material/MailOutline";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import "./Login.css";
-
 const Login = () => {
+  const [values, setValues] = useState({
+    email: "",
+    passwrd: "",
+    showPassword: false,
+  });
+
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:8000/users", {
+        email: values.email,
+        passwrd: values.passwrd,
+      })
+      .then((res) => console.log(res));
+  };
+  // console.log(values);
+  const handlePassVisibility = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
   return (
-    <Grid className="GridStyle">
-      <Paper className="PaperStyle">
+    <Container maxWidth="sm" className="GridStyle">
+      <Paper className="PaperStyle" elevation={4}>
         <Grid align="center">
           <Avatar className="AvatarStyle">
             <LockOutlinedIcon />
           </Avatar>
           <h1>Log In</h1>
         </Grid>
-        <TextField
-          className="TextFiledStyle"
-          fullWidth
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <MailOutlineIcon className="StartIcons" />
-              </InputAdornment>
-            ),
-          }}
-          required
-          variant="standard"
-          placeholder="Email"
-          type="email"
-        />
 
-        <TextField
-          className="TextFiledStyle"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <LockOutlinedIcon className="StartIcons" />
-              </InputAdornment>
-            ),
-            endAdornment: (
-              <InputAdornment className="EndIcons">
-                <VisibilityOffIcon fontSize="12px" />
-              </InputAdornment>
-            ),
-          }}
-          required
-          variant="standard"
-          placeholder="Password"
-          type="password"
-        />
+        <form onSubmit={handelSubmit}>
+          <TextField
+            onChange={(e) => setValues({ ...values, email: e.target.value })}
+            className="TextFiledStyle"
+            fullWidth
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <MailOutlineIcon className="StartIcons" />
+                </InputAdornment>
+              ),
+            }}
+            variant="standard"
+            placeholder="Email Adress"
+            type="email"
+          />
 
-        <Button fullWidth variant="outlined" type="submit">
-          Sign in
-        </Button>
+          <TextField
+            onChange={(e) => setValues({ ...values, passwrd: e.target.value })}
+            className="TextFiledStyle"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <LockOutlinedIcon className="StartIcons" />
+                </InputAdornment>
+              ),
+              endAdornment: (
+                <InputAdornment
+                  position="end"
+                  className="EndIcons"
+                  aria-label="toggle-password"
+                  edge="end"
+                >
+                  <IconButton onClick={handlePassVisibility}>
+                    {values.showPassword ? (
+                      <VisibilityIcon />
+                    ) : (
+                      <VisibilityOffIcon />
+                    )}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+            required
+            variant="standard"
+            placeholder="Password"
+            type={values.showPassword ? "text" : "password"}
+          />
 
+          <Button fullWidth variant="outlined" type="submit">
+            Sign in
+          </Button>
+        </form>
         <Typography align="center">
           <Link href="@" variant="body2">
             Forget Paswword?
           </Link>
         </Typography>
       </Paper>
-    </Grid>
+    </Container>
   );
 };
 
